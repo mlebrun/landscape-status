08cd2bb0d46e35ac902e42fbbc91d6e8385d877a
Matt LeBrun <lebrun@adobe.com>
2015-02-28 13:56:48 -0500

    Project: now hits Postgres instance

--- index.js
+++ index.js
@@ -1,8 +1,46 @@
 var express = require('express');
 var app = express();
+var pg = require('pg');
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
-})
+var port = process.env.PORT || 5000;
+var conString = process.env.HEROKU_POSTGRESQL_YELLOW_URL || false;
 
-app.listen(3000);
+app.post('/', function (req, res) {
+
+  console.log(req.query);
+
+  pg.connect(conString, function(err, client, done) {
+    if(err) {
+      return res.send('Error fetching client from pool');
+    }
+
+    client.query('SELECT * FROM landscapes', [], function(err, result) {
+      done(); // call `done()` to release the client back to the pool
+      client.end();
+
+      var list = '',
+          landscapes;
+
+      if(err) {
+        return res.send('There was an error running your query');
+      }
+
+      if (!result.rows.length) {
+        return res.send('No landscapes found');
+      }
+
+      landscapes = result.rows;
+
+      for (var i = landscapes.length - 1; i >= 0; i--) {
+        var row = landscapes[i];
+        list += row.landscape + ': ' + (row.locked_by || 'unlocked') + "\n";
+      }
+
+      return res.send(list);
+    });
+  });
+});
+
+app.listen(port);
+
+console.log( 'Running on' + port );
